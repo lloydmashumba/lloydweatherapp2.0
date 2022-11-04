bc8ae98e45bdccbb71b27a4ec1860ea348983329
lloyd <lloydmashumba@gmail.com>
2022-11-04 15:41:08 +0200

    Complete : Dashboard structure
    dashboard initialised with forest sunny theme

--- WeatherApp/ViewController/DashboardViewController.swift
+++ WeatherApp/ViewController/DashboardViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class DashboardViewController: UIViewController {
 
+    //MARK: dashboard views
+    @IBOutlet weak var themeImageViewContainer: UIView!
+    @IBOutlet weak var themeImageView: UIImageView!
+    @IBOutlet weak var currentWeatherContainerView: UIView!
+    @IBOutlet weak var tempRecordStackView: UIStackView!
+    
+    
+    //MARK: constraints
+    @IBOutlet weak var themeImageViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var tempRecordHeightConstraint: NSLayoutConstraint!
+    
+    
+    private var viewHeight : CGFloat {
+        UIScreen.main.bounds.height
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor(named: "forest_sunny")
+        themeImageView.image = UIImage(named: "forest_sunny")
+        
     }
 
+    //:- Setup constraints depending on the height view
+    override func viewWillAppear(_ animated: Bool) {
+        themeImageViewHeightConstraint.constant = viewHeight * 0.4
+        tempRecordHeightConstraint.constant = viewHeight * 1
+    }
 
 }
 
