3dcd806e96249ae432fe9c54c62441c427cfa454
lloyd <lloydmashumba@gmail.com>
2022-11-05 09:28:21 +0200

    Add : style to the current weather labels
    - to make them look more appealing

--- WeatherApp/ViewController/DashboardViewController.swift
+++ WeatherApp/ViewController/DashboardViewController.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class DashboardViewController: UIViewController {
+    
+    
+    //MARK: Properties
+    
+    var mainTempDescriptionStackView = UIStackView(frame: .init(x: 0, y: 0, width: 0, height: 0))
+
+    
+    var temperatureLabel = UILabel()
+    var weatherDescriptionLabel = UILabel()
+    
 
     //MARK: dashboard views
     @IBOutlet weak var themeImageViewContainer: UIView!
@@ -28,16 +38,54 @@ class DashboardViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        mainTempDescriptionStackView.axis = .vertical
+        mainTempDescriptionStackView.distribution = .fill
+        
+        temperatureLabel.text = "80"
+        weatherDescriptionLabel.text = "Sunny"
+        
+        
+        
         view.backgroundColor = UIColor(named: "forest_sunny")
         themeImageView.image = UIImage(named: "forest_sunny")
         
     }
 
-    //:- Setup constraints depending on the height view
+    //:- design dynamic set up
     override func viewWillAppear(_ animated: Bool) {
         themeImageViewHeightConstraint.constant = viewHeight * 0.4
         tempRecordHeightConstraint.constant = viewHeight * 1
+        
+        setUpCurrentTemperatureView()
     }
+    
+    //MARK: - setting up the temperature view
+    func setUpCurrentTemperatureView(){
+        
+        //:- styling temperature lable
+        temperatureLabel.textColor = .white
+        temperatureLabel.shadowColor = .black
+        temperatureLabel.shadowOffset = .init(width: 0.2, height: 0.3)
+        temperatureLabel.textAlignment = .center
+        temperatureLabel.font = .systemFont(ofSize: 60, weight: .heavy)
+        
+        //:- styling weather description label
+        weatherDescriptionLabel.textColor = .white
+        weatherDescriptionLabel.textAlignment = .center
+        weatherDescriptionLabel.font = .systemFont(ofSize: 40, weight: .bold)
+        
+        mainTempDescriptionStackView.addArrangedSubview(temperatureLabel)
+        mainTempDescriptionStackView.addArrangedSubview(weatherDescriptionLabel)
+        
+        currentWeatherContainerView.addSubview(mainTempDescriptionStackView)
+        
+        mainTempDescriptionStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        mainTempDescriptionStackView.centerXAnchor.constraint(equalTo: currentWeatherContainerView.centerXAnchor).isActive = true
+        mainTempDescriptionStackView.centerYAnchor.constraint(equalTo: currentWeatherContainerView.centerYAnchor).isActive = true
+    }
+    
+    
 
 }
 
